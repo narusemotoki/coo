8c65b5d31363689adfff1423d85e18d1be1c1973
Motoki Naruse <motoki@naru.se>
2021-06-27 00:41:55 +0900

    ファイル一覧のために読み込むディレクトリを設定ファイルから読み出すようにした

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Coo
 
+## 依存ライブラリをインストールする
+`$ sudo apt install libgtk2.0-dev libgtk-3-dev`
+
 ## ロガーを有効にする
 `$ RUST_LOG=debug cargo run`
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use gio::prelude::*;
 use gtk::prelude::*;
+use std::fs;
 
 mod views;
+
+fn expand_path(path: &str) -> String {
+    return shellexpand::tilde(path).into_owned();
+}
 
 fn bootstrap(application: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(application);
@@ -10,7 +15,10 @@ fn bootstrap(application: &gtk::Application) {
     window.set_position(gtk::WindowPosition::Center);
     window.set_default_size(1280, 720);
 
-    let files_and_file = views::files_and_file::FilesAndFile::new(".");
+    let config = fs::read_to_string(expand_path("~/.config/coo.toml")).unwrap();
+    let value = config.parse::<toml::Value>().unwrap();
+    let root = value["views"][0]["config"]["root"].as_str().unwrap();
+    let files_and_file = views::files_and_file::FilesAndFile::new(&expand_path(root));
 
     window.add(&files_and_file);
     window.show_all();
