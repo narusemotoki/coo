289756ac69b7aa5c5f44cdb3c0646cbc7a60dae0
Motoki Naruse <motoki@naru.se>
2021-07-10 01:19:58 +0900

    引数名を変更

--- src/libs.rs
+++ src/libs.rs
@@ -5,17 +5,17 @@ pub fn expand_path(path: &str) -> String {
 }
 
 pub fn find_first_child_by_name<T: glib::IsA<gtk::Widget>>(
-    parent: &gtk::Widget,
+    widget: &gtk::Widget,
     name: &str,
 ) -> Option<T> {
-    find_first_widget_by_name(parent, name).and_then(|widget| widget.downcast().ok())
+    find_first_widget_by_name(widget, name).and_then(|widget| widget.downcast().ok())
 }
 
 fn find_first_widget_by_name<T: glib::IsA<gtk::Widget>>(
-    parent: &T,
+    widget: &T,
     name: &str,
 ) -> Option<gtk::Widget> {
-    if let Ok(container) = parent.clone().dynamic_cast::<gtk::Container>() {
+    if let Ok(container) = widget.clone().dynamic_cast::<gtk::Container>() {
         for child in container.children() {
             if child.widget_name() == name {
                 return Some(child);
@@ -25,7 +25,7 @@ fn find_first_widget_by_name<T: glib::IsA<gtk::Widget>>(
             }
         }
     }
-    if let Ok(bin) = parent.clone().dynamic_cast::<gtk::Bin>() {
+    if let Ok(bin) = widget.clone().dynamic_cast::<gtk::Bin>() {
         if let Some(child) = bin.child() {
             if child.widget_name() == name {
                 return Some(child);
